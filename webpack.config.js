e23c69eb88df35f5ca48fd654ef4740994416259
Solomon Hawk <solomon.hawk@viget.com>
2016-01-11 12:21:47 -0500

    [update] Remove babel query from webpack config, it is present in .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ["es2015"],
-          plugins: ["transform-object-assign"]
-        }
+        loader: 'babel'
       }
     ]
   }
