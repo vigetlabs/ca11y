77841f2bb3548c39cf87c6a4012c3d696ff974c7
Solomon Hawk <solomon.hawk@viget.com>
2016-01-11 12:06:01 -0500

    [update] Add additional browsers to karma config, add plugins

--- karma.config.js
+++ karma.config.js
@@ -1,9 +1,8 @@
 module.exports = function (config) {
-
   return config.set({
     basePath: '.',
 
-    browsers: [ 'Chrome' ],
+    browsers: [ 'PhantomJS', 'Chrome', 'Firefox' ],
 
     frameworks: [ 'mocha' ],
 
@@ -14,11 +13,21 @@ module.exports = function (config) {
     ],
 
     preprocessors: {
-      './test/**/*.test.js*': ['webpack'],
-      '**/*.html': ['html2js']
+      './test/**/*.test.js*': [ 'webpack' ],
+      '**/*.html': [ 'html2js' ]
     },
 
     reporters: [ 'mocha' ],
+
+    plugins: [
+      'karma-webpack',
+      'karma-html2js-preprocessor',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-phantomjs-launcher',
+      'karma-mocha-reporter',
+      'karma-mocha'
+    ],
 
     webpack: require('./webpack.config'),
 
